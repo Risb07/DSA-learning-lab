88eab06413ba2798e5c591644a2756371eba2860
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-06-29 23:10:57 +0530

    Leetcode - 7. Reverse Integer - raw while loop, need optimization

--- Daily_DSA_practice/Leetcode_Maths/ReverseInteger.java
+++ Daily_DSA_practice/Leetcode_Maths/ReverseInteger.java
@@ -3,5 +3,26 @@
  */
 public class ReverseInteger {
 
-      
+    public int reverse(int x) {
+        long num = 0;
+        int n = Math.abs(x);
+
+        while(n>0){
+
+            int a = x%10;
+            num = num*10+a;
+            x=x/10;
+            n=n/10;
+        }
+        if(num>Integer.MAX_VALUE || num <Integer.MIN_VALUE)
+            return 0;
+        else
+            return (int)num;
+        
+    }
+    public static void main(String[] args) {
+      ReverseInteger r = new ReverseInteger();
+      System.out.println(r.reverse(1534236469));
+    }
 }
+      
