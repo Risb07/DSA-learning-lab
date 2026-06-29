b202d9c9e56cf5402f9c9edb7a49dc3062ba2098
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-06-29 23:13:04 +0530

    Leetcode - 50. Pow(x, n) ( Optimized )

---
+++ Daily_DSA_practice/Leetcode_Maths/Pow.java
@@ -0,0 +1,47 @@
+class Pow {
+    public double myPow(double x, int n) {
+        double pow= 1.00 ;
+        if( n == 0){
+            return 1.0000;
+        }
+        if(n > 0){
+        for(int i = 0 ; i< n ;i++){
+            pow= pow * x;
+        }
+      }else{
+            for(int i = 0 ; i < (n*(-1)); i++){
+                  pow = pow * x;
+            }
+            pow = 1/pow;
+      }
+        return pow;
+    }
+
+//optimized 
+    class Solution {
+    public double myPow(double x, int n) {
+       long num = n;
+       if(num < 0){
+            x=1/x;
+            num= -num;
+       }
+       double res = 1;
+
+       while(num > 0){
+
+        if((num & 1) == 1){
+            res*=x;
+        }
+
+        x*=x;
+        num/=2;
+       }
+       return res;
+    }
+}
+    public static void main(String[] args){
+      Pow p = new Pow();
+      System.out.println(p.myPow(1.0000,2147483647));
+    }
+    
+}
