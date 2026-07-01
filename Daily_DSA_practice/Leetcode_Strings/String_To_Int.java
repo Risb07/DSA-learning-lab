b3c0de6e34b5b68e14b8e7e321bf87a2e13d5492
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-07-01 08:27:37 +0530

    Leetcode - 8. String to Integer (atoi) (Attempt 2 - Correct Answer according to question 100% beats  )

--- Daily_DSA_practice/Leetcode_Strings/String_To_Int.java
+++ Daily_DSA_practice/Leetcode_Strings/String_To_Int.java
@@ -5,21 +5,46 @@ package Daily_DSA_practice.Leetcode_Strings;
  */
 public class String_To_Int {
 
-   int n=0 ;
+  
     public int myAtoi(String s) {
-        for(char c : s.toCharArray()){
-            
-            int a = c-'0';
+        s = s.trim();
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        int sign = 1;
+        int index = 0;
+      long n = 0;
+
+       if (s.charAt(0) == '-') {
+            sign = -1;
+            index++;
+        } else if (s.charAt(0) == '+') {
+            index++;
+        }
+          for (int i = index; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int a = c - '0';
+            if(a < 0 | a >9){
+                  break; 
+            }
             
             n = n*10 + a;
-            
-        }
-        return n;
+            if (sign == 1 && n > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && (-n) < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+           }
+        
+      
+      return (int) (n * sign);
     }
 
     public static void main(String[] args) {
       String_To_Int s = new String_To_Int();
-     System.out.println( s.myAtoi("42"));
+     System.out.println( s.myAtoi("-91283472332"));
     }
 }
       
