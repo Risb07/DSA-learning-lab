6253b189a74c9eb1ce20a4d3abe8c5ec81f92b07
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-07-08 02:29:04 +0530

    Patterns 1

---
+++ Daily_DSA_practice/Patterns/Pattern1.java
@@ -0,0 +1,34 @@
+/*
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+/**
+ * Pattern1
+ */
+public class Pattern1 {
+
+      public void Print(int n){
+           
+          
+            int cols = 0;
+            for(int i = 0 ; i < n*n ; i++){
+                  System.out.print('*');
+                  cols++;
+                  if (cols == n){
+                        cols = 0;
+                        System.out.println();
+                        continue;
+                  } 
+            }
+      }
+      public static void main(String[] args) {
+            Pattern1 p = new Pattern1();
+            p.Print(10);
+      }
+}
