71fc20bca6930c913ea13cb91f81b535a5b73ed6
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-07-08 02:48:48 +0530

    Patterns 2   -   right-angled-triangle-pattern

---
+++ Daily_DSA_practice/Patterns/Pattern2.java
@@ -0,0 +1,32 @@
+/*
+
+
+*
+**
+***
+****
+*****
+
+*/ 
+
+public class Pattern2 {
+      public void Print(int n ){
+
+            int cols = 0;
+            int  l = 1;
+            for(int i = 1 ; i <= (n *(n+1)) / 2 ; i++){
+                  System.out.print('*');
+                  cols++;
+                  if(cols == l){ 
+                        cols = 0;
+                        System.out.println();
+                        l++;
+                  }
+            }
+
+      }
+      public static void main(String[] args) {
+            Pattern2 p = new Pattern2();
+            p.Print(5);
+      }
+}
