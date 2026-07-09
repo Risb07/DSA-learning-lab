86cde7bce2788c8b6a018047ce9f2cbfc65699ae
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-07-10 00:28:25 +0530

    Leetcode   -   258. Add Digits, ( beats 97.91 , can be optimized  )

--- Daily_DSA_practice/Leetcode_Maths/Add_Digits.java
+++ Daily_DSA_practice/Leetcode_Maths/Add_Digits.java
@@ -1,3 +1,22 @@
 public class Add_Digits {
-      
+      public int addDigits(int num) {
+            int x = num;
+            // int result = 0;
+            while (x >= 10) {
+                  int result = 0;
+                  int n = x;
+                  while (n > 0) {
+                        result += n % 10;
+                        n = n / 10;
+                  }
+                  x = result;
+            }
+            return x;
+      }
+
+      public static void main(String[] args) {
+            
+            Add_Digits add = new Add_Digits();
+            System.out.println(add.addDigits(100));
+      }
 }
