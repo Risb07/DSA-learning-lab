d1cbd44e7143facb8cb1939a988a807bea06c4d6
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-07-11 23:53:33 +0530

    Leetcode  -   231. Power of Two ( 1 kind of solution, try more  )

---
+++ Daily_DSA_practice/Leetcode_Maths/Power.java
@@ -0,0 +1,27 @@
+/**
+ * Power
+ */
+public class Power {
+
+      public boolean isPowerOfTwo(int n) {
+            if (n == 1) {
+                  return true;
+            }
+            if (n <=0){
+                  return false;
+            }
+            while (n > 1) {
+                  if (n % 2 == 0 )
+                        n = n / 2;
+                  else
+                        return false;
+            }
+            return true;
+      }
+      
+      public static void main(String[] args) {
+            Power p = new Power();
+            System.out.println(p.isPowerOfTwo(-16));
+      }
+
+}
