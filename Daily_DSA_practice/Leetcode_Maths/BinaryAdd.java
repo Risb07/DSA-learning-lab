603c32d16f0e0ada1bb09c6cc8996e49d12ab724
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-08-09 23:29:27 +0530

    Binary Numbers Addition  (TO Do )

---
+++ Daily_DSA_practice/Leetcode_Maths/BinaryAdd.java
@@ -0,0 +1,11 @@
+class BinaryAdd{
+          public static void addBinary(String a, String b) {
+         
+         char[] a1 = a.toCharArray();
+         char[] a2 = b.toCharArray();
+    }
+
+    public static void main(String[] args) {
+            addBinary("11", "1010");
+    }
+}
