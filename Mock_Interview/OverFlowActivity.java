536bc63cfa13533825054fe78cf932c1d4d23cca
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-08-09 23:34:43 +0530

    Mock Interview :  Count Non Overflow Activities not between Start and End

---
+++ Mock_Interview/OverFlowActivity.java
@@ -0,0 +1,16 @@
+package Mock_Interview;
+
+import java.util.HashMap;
+
+public interface OverFlowActivity {
+      public static void activities(int [][] arr){
+        HashMap<Integer, Integer> map = new HashMap<>();
+       
+        
+    }
+
+      public static void main(String[] args) {
+            int[][] arr = new int[][] { { 1, 4 }, { 3, 5 }, { 0, 6 } };
+            activities(arr);
+      }
+}
