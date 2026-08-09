43c8caaa2bf3b3045bfac5a3adc3d11f0087d6dc
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-08-09 23:27:09 +0530

    Reversing an Array (To Do )

---
+++ Daily_DSA_practice/Hackerrank_Arrays/Reverse_Array.java
@@ -0,0 +1,14 @@
+package Daily_DSA_practice.Hackerrank_Arrays;
+
+import java.util.List;
+
+public class Reverse_Array {
+      public static List<Integer> reverseArray(List<Integer> a) {
+            // int[] arr = a.toArray(new int[0]::new);
+
+            // for(int i = 0 ; i <)
+
+            return a;
+      }
+
+}
