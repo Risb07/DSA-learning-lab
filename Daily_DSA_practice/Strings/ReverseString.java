955bda34c106a010ed70d79a37bf57a61d772aaa
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-08-21 23:24:13 +0530

    String : Reverse String

--- Daily_DSA_practice/Strings/ReverseString.java
+++ Daily_DSA_practice/Strings/ReverseString.java
@@ -3,14 +3,16 @@ package Daily_DSA_practice.Strings;
 public class ReverseString {
       
       public static void reverse(String str){
-            int n = str.length();
+            int n = str.length()-1;
             char[] charr = str.toCharArray();
             String s = "";
-            for(int i = 0 ; i < n ; i++){
+            for(int i = n ; i >=0 ; i--){
                   s = s+charr[i];
             }
-            
+            System.out.println(s);
             if(s.equals(str));
-
+      }
+      public static void main(String[] args) {
+            reverse("hello");
       }
 }
