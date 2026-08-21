d65d70f575841b2753712a6779d40fbccbf7dbe9
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-08-21 23:23:14 +0530

    Rotate Array (optimized using reverse approach) 3 step approach

--- Daily_DSA_practice/Arrays/Rotate.java
+++ Daily_DSA_practice/Arrays/Rotate.java
@@ -1,15 +1,19 @@
-import java.util.Arrays;
+
 
 /**
  * Rotate
  */
+package Daily_DSA_practice.Arrays;
+
+import java.util.Arrays;
+
 public class Rotate {
 
-
       /**
-       * complexity : time limit exceeds exception on leetcode while using below program 
+       * complexity : time limit exceeds exception on leetcode while using below
+       * program
        * 
-       * time  - O(k*n)
+       * time - O(k*n)
        * need to optimize it !
        */
       public void rotate(int[] nums, int k) {
@@ -27,41 +31,39 @@ public class Rotate {
 
       public void rotate(int[] arr) {
             int n = arr.length;
-            int temp = arr[n-1];
-            for (int i = n-1; i >0; i--) {
-                  arr[i] = arr[i-1];
+            int temp = arr[n - 1];
+            for (int i = n - 1; i > 0; i--) {
+                  arr[i] = arr[i - 1];
             }
             arr[0] = temp;
       }
 
-
       /**
-       * Optimizing code using reverse approach 
+       * Optimizing code using reverse approach
        * Time - O(n)
        * step 1 : divide the array in half
        * step 2 : reverse the both parts of array
        * step 3 : reverse the whole array.
        */
 
-      public void rotateWithReverse(int[] arr, int k){
+      public void rotateWithReverse(int[] arr, int k) {
 
             k = k % arr.length;
-            if(k<0)
+            if (k < 0)
                   k = k + arr.length;
-                  
 
-            reverse(arr, 0, k-1);
-            reverse(arr, k, arr.length-1);
-            reverse(arr, 0, arr.length-1);
+            reverse(arr, 0, k - 1);
+            reverse(arr, k, arr.length - 1);
+            reverse(arr, 0, arr.length - 1);
 
       }
 
-      public void reverse(int[] arr, int start , int end){
+      public void reverse(int[] arr, int start, int end) {
 
-            while(start < end){
+            while (start < end) {
                   int temp = arr[start];
                   arr[start] = arr[end];
-                  arr[end]=temp;
+                  arr[end] = temp;
                   start++;
                   end--;
             }
@@ -78,11 +80,9 @@ public class Rotate {
             if (k < 0)
                   k = k + arr.length;
 
-            reverse(arr, k - 1,0);
+            reverse(arr, k - 1, 0);
             reverse(arr, k, arr.length - 1);
             reverse(arr, 0, arr.length - 1);
-
-
 
       }
 
