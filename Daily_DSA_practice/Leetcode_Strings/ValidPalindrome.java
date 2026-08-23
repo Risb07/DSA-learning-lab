a2ff0b7ae584963848666eb7277cda8ec7d59dc5
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-08-23 22:42:10 +0530

    Leetcode - 125. Valid Palindrome   - Using s.replaceAll("[^a-zA-Z0-9]", "") ,  Character.isLetter(char c) ( more Efficient )

--- Daily_DSA_practice/Leetcode_Strings/ValidPalindrome.java
+++ Daily_DSA_practice/Leetcode_Strings/ValidPalindrome.java
@@ -4,6 +4,12 @@ package Daily_DSA_practice.Leetcode_Strings;
  * 125. Valid Palindrome
  */
 public class ValidPalindrome {
+
+      /*
+       * Using s.replaceAll("[^a-zA-Z0-9]", "") is inefficient because it forces Java
+       * to scan the text multiple times, compile complex internal patterns, and
+       * create massive amounts of duplicate data in memory.
+       */
       public static boolean isPalindrome(String s) {
             String s1 = s.replaceAll("[^a-zA-Z0-9]", "");
             if (s1.isEmpty()) {
@@ -19,7 +25,36 @@ public class ValidPalindrome {
             return true;
       }
 
+      /*
+       * Character.isLetter(char c): Returns true only if the character is an
+       * uppercase or lowercase alphabetical letter (a-z, A-Z). It returns false for
+       * numbers 0-9.
+       */
+
+      public static boolean isPalindrome2(String s) {
+            int left = 0;
+            int right = s.length() - 1;
+
+            while (left < right) {
+                  char start = s.charAt(left);
+                  char end = s.charAt(right);
+
+                  if (!Character.isLetter(start)) {
+                        left++;
+                  } else if (!Character.isLetter(end)) {
+                        right--;
+                  } else {
+                        if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
+                              return false;
+                        }
+                        left++;
+                        right--;
+                  }
+            }
+            return true;
+      }
+
       public static void main(String[] args) {
-            System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+            System.out.println(isPalindrome2("0P"));
       }
 }
