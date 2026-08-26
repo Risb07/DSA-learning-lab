407d50481903560f55f0039410f2ffc866cdb7a2
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-08-26 23:48:29 +0530

    Leetcode - 14. Longest_Common_Prefix ( 100% beats , 0ms ) using String.startsWith() method

---
+++ Daily_DSA_practice/Leetcode_Arrays/Longest_Common_Prefix.java
@@ -0,0 +1,27 @@
+package Daily_DSA_practice.Leetcode_Arrays;
+
+import java.util.HashMap;
+
+public class Longest_Common_Prefix {
+      public String longestCommonPrefix(String[] strs) {
+            if (strs.length == 0) {
+                  return "";
+            }
+            String prefix = strs[0];
+            for (int i = 0; i < strs.length; i++) {
+                  if (strs[i].length() > 0 || prefix.length() > 0) {
+                        while (!strs[i].startsWith(prefix)) {
+                              prefix = prefix.substring(0, prefix.length() - 1);
+                        }
+                  }
+
+            }
+            return prefix;
+      }
+
+      public static void main(String[] args) {
+            Longest_Common_Prefix l = new Longest_Common_Prefix();
+            System.out.println(l.longestCommonPrefix(new String[] { "abab", "aba", "" }));
+      }
+
+}
