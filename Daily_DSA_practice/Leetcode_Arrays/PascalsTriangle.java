0d657f0dfbe278a0ec061a27413c04ae57b6d7f0
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-08-27 23:55:33 +0530

    Leetcode : Pascal Triangle

---
+++ Daily_DSA_practice/Leetcode_Arrays/PascalsTriangle.java
@@ -0,0 +1,34 @@
+package Daily_DSA_practice.Leetcode_Arrays;
+
+public class PascalsTriangle {
+      public static int[][] pascalT(int a) {
+            if (a < 1) {
+                  return new int[0][];
+            }
+            if (a == 1) {
+                  return new int[][] { { 1 } };
+            }
+            int[][] arr = new int[a][];
+            arr[0] = new int[] { 1 };
+            arr[1] = new int[] { 1, 1 };
+            for (int i = 2; i < a; i++) {
+                  arr[i] = new int[i + 1];
+                  arr[i][0] = 1;
+                  arr[i][i] = 1;
+                  for (int j = 1; j < i; j++) {
+                        arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+                  }
+            }
+            return arr;
+      }
+
+      public static void main(String[] args) {
+            int[][] arr = pascalT(5);
+            for (int i = 0; i < arr.length; i++) {
+                  for (int j = 0; j < arr[i].length; j++) {
+                        System.out.print(arr[i][j] + " ");
+                  }
+                  System.out.println("");
+            }
+      }
+}
