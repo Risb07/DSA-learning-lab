7f86bf817aa5b0b83eddfe2949a05fe1059cad17
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-09-03 23:16:09 +0530

    Leetcode : 83. Remove Duplicates from Sorted List ( not fully optimized )

--- Daily_DSA_practice/Leetcode_LinkedList/RemoveDuplicatesFromSortedList.java
+++ Daily_DSA_practice/Leetcode_LinkedList/RemoveDuplicatesFromSortedList.java
@@ -1,5 +1,54 @@
 package Daily_DSA_practice.Leetcode_LinkedList;
 
+import java.util.HashSet;
+
+// Definition for singly-linked list.
+class LinkedListNode {
+      int val;
+      LinkedListNode next;
+
+      LinkedListNode() {
+      }
+
+      LinkedListNode(int val) {
+            this.val = val;
+      }
+
+      LinkedListNode(int val, LinkedListNode next) {
+            this.val = val;
+            this.next = next;
+      }
+}
+
 public class RemoveDuplicatesFromSortedList {
-      
+
+      public LinkedListNode deleteDuplicates(LinkedListNode head) {
+            if (head == null) {
+                  return head;
+            }
+
+            LinkedListNode node = head;
+            LinkedListNode node2 = node.next;
+
+            HashSet<Integer> set = new HashSet<>();
+
+            set.add(head.val);
+
+            while (node2 != null) {
+                  if (set.contains(node2.val)) {
+                        node.next = node2.next;
+                        if (node2 != null)
+                              node2 = node2.next;
+                  } else {
+                        set.add(node2.val);
+                        node = node.next;
+                        node2 = node2.next;
+                  }
+            }
+            return head;
+      }
+
+      public static void main(String[] args) {
+
+      }
 }
